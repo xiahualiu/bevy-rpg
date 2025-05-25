ad78b6d2da648c1d024a5ba6c989c54368b1bc3f
Xiahua Liu <49677912+xiahualiu@users.noreply.github.com>
2025-05-24 17:26:12 -0700

    Add loading plugin

--- src/loading.rs
+++ src/loading.rs
@@ -1,28 +1,23 @@
 use bevy::prelude::*;
 
+use crate::GameState;
+
 // This plugin loads all assets using vanilla bevy
 pub struct LoadingPlugin;
 
-// Separate the loading state from the game state
-#[derive(States, Debug, Clone, Default, Eq, PartialEq, Hash)]
-enum LoadingState {
-    #[default]
-    Loading,
-    Ready,
-}
-
 // Texture atlas layout handle
-#[derive(Resource)]
+#[derive(Resource, Default)]
 struct RpgAtlasLayout(Handle<TextureAtlasLayout>);
 
 // Texture atlas handle
-#[derive(Resource)]
+#[derive(Resource, Default)]
 struct RpgTextureAtlas(Handle<Image>);
 
 impl Plugin for LoadingPlugin {
     fn build(&self, app: &mut App) {
-        app.init_state::<LoadingState>()
-        .add_systems(OnEnter(LoadingState::Loading), load_texture_atlas);
+        app.init_resource::<RpgAtlasLayout>()
+            .init_resource::<RpgTextureAtlas>()
+            .add_systems(OnEnter(GameState::Loading), load_texture_atlas);
     }
 }
 
@@ -33,7 +28,7 @@ fn load_texture_atlas(
     mut texture_res: ResMut<RpgTextureAtlas>,
 ) {
     // Load the texture atlas
-    texture_res.0 = asset_server.load("assets/textures/terrain.png");
+    texture_res.0 = asset_server.load("textures/terrain.png");
     layout_res.0 = asset_layout.add(TextureAtlasLayout::from_grid(
         UVec2::splat(16),
         16,
